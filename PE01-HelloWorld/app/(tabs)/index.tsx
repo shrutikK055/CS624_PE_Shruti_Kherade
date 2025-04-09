78efe248842d2e479b6aa696fabd3446feacba13
shrutikK055 <shrutikherde23@gmail.com>
2025-04-09 20:36:32 +0000

    Submission for PE01 - Shruti Kherade

---
+++ PE01-HelloWorld/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Shruti</Text>
+      <Text style={styles.text}>MSCS</Text>
+      <Text style={styles.text}>STC</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    margin: 10,
+  },
+});
